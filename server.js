ec083c700603de04dfa5d788de9a7b7a3b8670b5
Jus-smi <112442098+Jus-smi@users.noreply.github.com>
2022-10-28 13:40:56 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require("http");
 http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write("<h1>Hello, Node.js</h1>");
-    response.write("<h2>this</h2>")
+    response.write("<h2>Justin Smith - t87g863</h2>/
+                   <p>Running node server on port 3067</p>");
     response.end();
 }).listen(3067);
